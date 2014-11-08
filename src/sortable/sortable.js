372a2b92e1a94c64d5b316ac8438e6c6b498bf71
Julien Reboul <julien.reboul@bonitasoft.com>
2014-11-08 03:23:51 +0100

    add sortable template to angular template cache

--- src/sortable/sortable.js
+++ src/sortable/sortable.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
   angular
-  .module('bonita.sortable',[])
+  .module('bonita.sortable',['template/sortable/sorter.tpl.html'])
   .directive('boSorter', function(){
     return {
       restrict: 'A',
@@ -30,4 +30,10 @@
       }
     };
   });
+
+  angular.module('template/sortable/sorter.tpl.html', []).run(['$templateCache', function($templateCache) {
+    $templateCache.put('template/sortable/sorter.tpl.html',
+      '<span ng-click=\"sort()\" ng-transclude></span>'+
+      '<span ng-click=\"sort()\" class=\"glyphicon\" ng-class=\"{\'glyphicon-chevron-up\':sortOptions.ascendant &amp;&amp; sortOptions.property === property, \'glyphicon-chevron-down\':!sortOptions.ascendant &amp;&amp; sortOptions.property === property}\"></i>');
+  }]);
 })();
