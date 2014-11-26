a3f433c1486d87e3c875e9f382f0c01a1a6574f0
Lionel Breduillieard <lionel@lumographe.fr>
2014-11-26 15:40:22 +0100

    ng-sortable dependency

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
       'bower_components/angular/*.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/ng-sortable/dist/ng-sortable.js',
       'src/**/*.js',
       'test/**/*.js',
       'src/**/*.html'
