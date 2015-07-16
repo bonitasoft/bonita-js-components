49623f62471aa0d0b5f0b85df1d3399b931c5bbb
ntith <nicolas.tith@bonitasoft.com>
2015-07-16 17:43:32 +0200

    BS-10673 store bonitable configuration into local storage Manage storage id

--- src/storable/storable.js
+++ src/storable/storable.js
@@ -8,37 +8,48 @@ angular
   .directive('boStorable', function($localStorage){
     return {
       restrict: 'A',
+
       require:'^bonitable',
       priority: 1,
       link: function(scope,elt, attr, bonitableCtrl) {
+        var storageId = attr.boStorable;
+        if(!storageId){
+          throw new Error('you must set a storageId to bo-storable');
+        }
+
 
         scope.init = function init(){
-          if($localStorage.columns){
-            scope.$columns = $localStorage.columns;
+          if(!$localStorage[storageId]){
+            $localStorage[storageId] = {};
           }
-          
-          if($localStorage.sortOptions){
-            bonitableCtrl.setOptions($localStorage.sortOptions);
+          if($localStorage[storageId].columns){
+            scope.$columns = $localStorage[storageId].columns;
+          }else{
+            $localStorage[storageId].columns = null;
           }
-
+          if($localStorage[storageId].sortOptions){
+            bonitableCtrl.setOptions($localStorage[storageId].sortOptions);
+          }else{
+            $localStorage[storageId].sortOptions = null;
+          }
         };
 
         scope.clearLocalStorage = function init(){
           //TODO
 
         };
-        
+
         scope.init();
 
         scope.$watch(bonitableCtrl.getOptions, function(newValue, oldValue) {
           if (newValue !== oldValue) {
-            $localStorage.sortOptions = newValue;
+            $localStorage[storageId].sortOptions = newValue;
           }
         }, true);
 
         scope.$watch('$columns', function(newValue, oldValue) {
           if (newValue !== oldValue) {
-            $localStorage.columns = newValue;
+            $localStorage[storageId].columns = newValue;
           }
         }, true);
       }
