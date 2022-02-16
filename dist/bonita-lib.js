b6fd4c2cc7128bb315f428f96d34e18e3a1a7b03
abirembaut <anthony.birembaut@gmail.com>
2022-02-16 17:30:15 +0100

    Release 0.6.0

--- dist/bonita-lib.js
+++ dist/bonita-lib.js
@@ -1116,6 +1116,69 @@ angular
   };
 });
 
+/**
+ * Created by fabiolombardi on 15/07/2015.
+ */
+angular
+    .module('org.bonitasoft.bonitable.storable', [
+        'org.bonitasoft.bonitable',
+        'ngStorage'
+    ])
+    .directive('boStorable', ['$localStorage', function($localStorage) {
+        return {
+            restrict: 'A',
+            require: '^bonitable',
+            priority: 1,
+            link: function(scope, elt, attr, bonitableCtrl) {
+                var storageId = attr.boStorable;
+                if (!storageId) {
+                    throw new Error('you must set a storageId to bo-storable');
+                }
+
+                scope.clearTableStorage = function clearTableStorage(storageId) {
+                    delete $localStorage[storageId];
+                };
+
+                if (!$localStorage[storageId]) {
+                    $localStorage[storageId] = {};
+                }
+                if ($localStorage[storageId].columns) {
+                    scope.$columns = $localStorage[storageId].columns;
+                } else {
+                    $localStorage[storageId].columns = null;
+                }
+                if ($localStorage[storageId].sortOptions) {
+                    bonitableCtrl.getOptions().property = $localStorage[storageId].sortOptions.property;
+                    bonitableCtrl.getOptions().direction = $localStorage[storageId].sortOptions.direction;
+                } else {
+                    $localStorage[storageId].sortOptions = null;
+                }
+                if ($localStorage[storageId].itemsPerPage) {
+                  scope.pagination.itemsPerPage = $localStorage[storageId].itemsPerPage;
+                } else {
+                  $localStorage[storageId].itemsPerPage = null;
+                }
+
+
+                scope.$watch(bonitableCtrl.getOptions, function(newValue) {
+                    $localStorage[storageId].sortOptions = newValue;
+                }, true);
+
+                scope.$watch('$columns', function(newValue, oldValue) {
+                    if (newValue !== oldValue) {
+                        $localStorage[storageId].columns = newValue;
+                    }
+                }, true);
+
+                scope.$watch('pagination.itemsPerPage', function(newValue) {
+                  $localStorage[storageId].itemsPerPage = newValue;
+                }, true);
+
+                bonitableCtrl.onStorageLoaded();
+            }
+        };
+    }]);
+
 angular
   .module('org.bonitasoft.bonitable.sortable',['org.bonitasoft.bonitable'])
   /**
@@ -1244,69 +1307,6 @@ angular
       }
     };
   });
-
-/**
- * Created by fabiolombardi on 15/07/2015.
- */
-angular
-    .module('org.bonitasoft.bonitable.storable', [
-        'org.bonitasoft.bonitable',
-        'ngStorage'
-    ])
-    .directive('boStorable', ['$localStorage', function($localStorage) {
-        return {
-            restrict: 'A',
-            require: '^bonitable',
-            priority: 1,
-            link: function(scope, elt, attr, bonitableCtrl) {
-                var storageId = attr.boStorable;
-                if (!storageId) {
-                    throw new Error('you must set a storageId to bo-storable');
-                }
-
-                scope.clearTableStorage = function clearTableStorage(storageId) {
-                    delete $localStorage[storageId];
-                };
-
-                if (!$localStorage[storageId]) {
-                    $localStorage[storageId] = {};
-                }
-                if ($localStorage[storageId].columns) {
-                    scope.$columns = $localStorage[storageId].columns;
-                } else {
-                    $localStorage[storageId].columns = null;
-                }
-                if ($localStorage[storageId].sortOptions) {
-                    bonitableCtrl.getOptions().property = $localStorage[storageId].sortOptions.property;
-                    bonitableCtrl.getOptions().direction = $localStorage[storageId].sortOptions.direction;
-                } else {
-                    $localStorage[storageId].sortOptions = null;
-                }
-                if ($localStorage[storageId].itemsPerPage) {
-                  scope.pagination.itemsPerPage = $localStorage[storageId].itemsPerPage;
-                } else {
-                  $localStorage[storageId].itemsPerPage = null;
-                }
-
-
-                scope.$watch(bonitableCtrl.getOptions, function(newValue) {
-                    $localStorage[storageId].sortOptions = newValue;
-                }, true);
-
-                scope.$watch('$columns', function(newValue, oldValue) {
-                    if (newValue !== oldValue) {
-                        $localStorage[storageId].columns = newValue;
-                    }
-                }, true);
-
-                scope.$watch('pagination.itemsPerPage', function(newValue) {
-                  $localStorage[storageId].itemsPerPage = newValue;
-                }, true);
-
-                bonitableCtrl.onStorageLoaded();
-            }
-        };
-    }]);
 
 'use strict';
 /**
